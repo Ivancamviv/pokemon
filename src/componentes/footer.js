8a867c03d1357224062762a06c900f74cb26acba
Ivan Dario Camacho <ivancamachoviveros@gmail.com>
2022-03-30 21:12:37 +0200

    subir appa react

---
+++ src/componentes/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Creado por <a href="#">Ivan Dario Camacho</a>
+    </div>
+    <div>
+      <a href="#">Ivan Dario Camacho V.</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
